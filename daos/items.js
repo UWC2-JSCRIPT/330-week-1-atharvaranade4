954acfc392070dc51fdd438a58d53494b354170c
Atharva Ranade <atharvaranade4@gmail.com>
2023-04-18 00:46:37 -0700

    finish HW

--- daos/items.js
+++ daos/items.js
@@ -11,14 +11,26 @@ module.exports.getAll = () => {
 
 module.exports.getById = (itemId) => {
   // TODO: complete
+  let itembyId = itemsModel.items.find(item => item.id === itemId)
+  return itembyId
 }
 
 module.exports.deleteById = async (itemId) => {
     // TODO: complete
+    let resultItems = itemsModel.items.filter(item => item.id !== itemId)
+    itemsModel.items = [...resultItems]
 }
 
 module.exports.updateById = async (itemId, newObj) => {
     // TODO: complete
+    let itemToUpdate = itemsModel.items.find(item => item.id === itemId);
+    if (itemToUpdate) {
+      for (let propName in newObj) {
+        if (propName === "id")
+          continue;
+        itemToUpdate[propName] = newObj[propName];
+      }
+    }
 }
 
 module.exports.create = async (item) => {
